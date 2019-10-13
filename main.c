510d6145be960318b3224b38daf82120d591dde1
Arseniy Obolenskiy <gooddoog@student.su>
2019-10-13 11:16:37 +0300

    Introduce trace_assert

--- main.c
+++ main.c
@@ -4,46 +4,32 @@
 const int width = 640;
 const int height = 480;
 
+#define trace_assert(expression)                                                            \
+    if (!(expression)) {                                                                    \
+        fprintf(stderr, "Assertion failed: %s at %s:%d", #expression, __FILE__, __LINE__);  \
+        exit(1);                                                                            \
+    }                                                                                       \
+
 int main(int argc, char **argv) {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        fprintf(stderr, "Could not initialize SDL: %s\n", SDL_GetError());
-        return 1;
-    }
+    trace_assert(SDL_Init(SDL_INIT_VIDEO) == 0);
 
-    SDL_Window *window = SDL_CreateWindow("Tetris",
+    SDL_Window *window = NULL;
+    trace_assert(window = SDL_CreateWindow("Tetris",
             SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-            width, height, SDL_WINDOW_SHOWN);
-    if (window == NULL) {
-        fprintf(stderr, "Could not create SDL window: %s\n", SDL_GetError());
-        return 1;
-    }
+            width, height, SDL_WINDOW_SHOWN));
+    SDL_Renderer *renderer = NULL;
+    trace_assert(renderer = SDL_CreateRenderer(window, -1,
+            SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC));
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
-            SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (renderer == NULL) {
-        fprintf(stderr, "Could not create SDL renderer Error: %s", SDL_GetError());
-        return 1;
-    }
-
-    SDL_Surface *surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
-    if (surface == NULL) {
-        fprintf(stderr, "Could not create SDL surface: %s", SDL_GetError());
-        return 1;
-    }
+    SDL_Surface *surface = NULL;
+    trace_assert(surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0));
     SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 140, 140, 140));
 
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
-    if (texture == NULL) {
-        fprintf(stderr, "Could not create texture from surface: %s\n", SDL_GetError());
-        exit(1);
-    }
+    SDL_Texture *texture = NULL;
+    trace_assert(texture = SDL_CreateTextureFromSurface(renderer, surface));
+    SDL_Surface *cell_surface = NULL;
+    trace_assert(cell_surface = SDL_CreateRGBSurface(0, 32, 32, 32, 0, 0, 0, 0));
 
-    SDL_Surface *cell_surface = SDL_CreateRGBSurface(0, 32, 32, 32, 0, 0, 0, 0);
-    if (cell_surface == NULL) {
-        fprintf(stderr, "Could not create SDL surface: %s", SDL_GetError());
-        return 1;
-    }
-    
     SDL_FillRect(cell_surface, NULL, SDL_MapRGB(cell_surface->format, 140, 140, 140));
     SDL_Rect border;
     border.x = border.y = 0;
@@ -59,11 +45,8 @@ int main(int argc, char **argv) {
     border.x = 32 - 2;
     SDL_FillRect(cell_surface, &border, SDL_MapRGB(cell_surface->format, 255, 255, 255));
 
-    SDL_Texture *cell_texture = SDL_CreateTextureFromSurface(renderer, cell_surface);
-    if (cell_texture == NULL) {
-        fprintf(stderr, "Could not create texture from surface: %s\n", SDL_GetError());
-        exit(1);
-    }
+    SDL_Texture *cell_texture = NULL;
+    trace_assert(cell_texture = SDL_CreateTextureFromSurface(renderer, cell_surface));
     
     SDL_Rect rect;
     rect.x = rect.y = 0;
