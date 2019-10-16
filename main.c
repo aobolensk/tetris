d4cc10dc57c9b5fa69de2069400803cb2d1ed895
Arseniy Obolenskiy <gooddoog@student.su>
2019-10-16 11:34:36 +0300

    Fix include file on macOS

--- main.c
+++ main.c
@@ -2,7 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#ifndef __APPLE__
 #include <SDL2/SDL.h>
+#else
+#include <SDL.h>
+#endif  // __APPLE__
 
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
